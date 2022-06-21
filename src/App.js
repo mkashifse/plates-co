a76293bf58131615ec1c5767627cd44a587a363d
Muhammad Kashif <mk.kashif.se@gmail.com>
2022-06-21 21:09:44 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+class Product {
+  constructor(_name, _code, _price, _qty) {
+    this.name = _name;
+    this.code = _code;
+    this.price = _price;
+    this.quantity = _qty;
+  }
+}
+
+const products = [
+  new Product("Red Plate", "R01", 32.95),
+  new Product("Green Plate", "G01", 24.95),
+  new Product("Blue Plate", "B01", 7.95),
+];
+
+const costs = {
+  lessThan50: 4.95,
+  lessThan90: 2.95,
+  greaterThan90: 0,
+};
+
+const offers = ["Buy one red get second half price"];
+
+const basket = [];
+
+const addToBasket = (product) => {
+  basket.push(product);
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>Salam</div>;
 }
 
 export default App;
